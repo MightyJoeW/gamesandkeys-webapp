97920b77abaf3252514b1a4bea5d8aae3dedc2aa
Joe Warren <joe.warren7@gmail.com>
2018-01-27 23:51:36 -0600

    chore: add and link dashboard js and css

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Dashboard.css";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Dashboard </h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 
 //COMPONENTS
 import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
 import NotFound from "./components/NotFound/NotFound";
 
 export default (
   <Switch>
     <Route exact path="/" component={Landing} />
+    <Route path="/dashboard" component={Dashboard} />
     <Route path="*" component={NotFound} />
   </Switch>
 );
