df40347c0280609f42238bf017b1a73acc593d35
Joe Warren <joe.warren7@gmail.com>
2018-01-28 19:21:56 -0600

    chore: setup dashboard grid layout

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,7 +5,25 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1> Dashboard </h1>
+        <div className="container">
+          <header>
+            <div class="nav-wrapper">
+              <div>Logo</div>
+              <div className="nav-right">About</div>
+              <div>Downloads</div>
+              <div>Requests</div>
+            </div>
+          </header>
+          <content>Tutorial List</content>
+          <section>Info</section>
+          <footer>
+            <div>Footer</div>
+            <div>Footer Col 2</div>
+            <div>Footer Col 3</div>
+            <div>Footer Col 4</div>
+            <div>Footer Col 5</div>
+          </footer>
+        </div>
       </div>
     );
   }
