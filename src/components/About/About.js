c7dde69df50fc0cb5ec8684e79f8b0b38cfe83f9
Joe Warren <joe.warren7@gmail.com>
2018-02-03 11:12:17 -0600

    style: setup basic responsive requests form

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => (
   <div>
-    <h1> About </h1>
+    <h1 className="views-title"> About </h1>
   </div>
 );
 
--- src/components/Requests/Requests.js
+++ src/components/Requests/Requests.js
@@ -1,8 +1,21 @@
 import React from "react";
+import "./Requests.css";
 
 const Requests = () => (
   <div>
-    <h1> Requests </h1>
+    <h1 className="views-title"> Requests </h1>
+    <form className="form-wrapper">
+      <label className="form-labels">Song Title:</label>
+      <input className="form-inputs" type="text" name="name" required />
+
+      <label className="form-labels">Artist/Composer:</label>
+      <input className="form-inputs" type="text" name="name" />
+
+      <label className="form-labels">Link to Original:</label>
+      <input className="form-inputs" type="text" name="name" />
+
+      <input id="form-button" type="submit" value="Submit" />
+    </form>
   </div>
 );
 
