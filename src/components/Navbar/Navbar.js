9b61960aadbe3d543442330d51c8123fc6ba305d
Joe Warren <joe.warren7@gmail.com>
2018-02-02 21:28:33 -0600

    chore: Link Navbar routes to components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "./Navbar.css";
 
+import { Link } from "react-router-dom";
+
 const Navbar = () => (
   <div className="nav-wrapper">
     <div className="nav">
-      <div className="nav-home">Home</div>
-      <div className="nav-right">Downloads</div>
-      <div className="nav-right">Requests</div>
-      <div className="nav-right">About</div>
+      <div className="nav-home">
+        <Link
+          to="/dashboard"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          Home
+        </Link>
+      </div>
+      <div className="nav-right">
+        <Link
+          to="/downloads"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          Downloads
+        </Link>
+      </div>
+      <div className="nav-right">
+        <Link to="/requests" style={{ textDecoration: "none", color: "black" }}>
+          Requests
+        </Link>
+      </div>
+      <div className="nav-right">
+        <Link to="/about" style={{ textDecoration: "none", color: "black" }}>
+          About
+        </Link>
+      </div>
       <button className="nav-button" type="button">
         Menu
       </button>
