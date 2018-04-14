fa1a1e117e2c718cdad67969375066104f67237c
Joe Warren <joe.warren7@gmail.com>
2018-04-13 23:08:40 -0500

    chore: install redux dependencies and add create state folder

---
+++ src/state/README.md
@@ -0,0 +1,1 @@
+<!-- State tree will go here -->
---
+++ src/state/index.js
@@ -0,0 +1,11 @@
+// External Dependencies
+
+// Local Dependencies
+
+// Create the root reducer
+
+// Include redux dev tools in the store enhancer if on development
+
+// Render redux dev tools if on development
+
+// Creates a redux store
