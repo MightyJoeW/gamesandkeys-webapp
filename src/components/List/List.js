901c754265363d6b3a0d4ffb98eeac1758c69e60
Joe Warren <joe.warren7@gmail.com>
2018-04-19 23:12:29 -0500

    style: flex card and remove links text

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -10,12 +10,11 @@ import Typography from 'material-ui/Typography';
 import preload from '../../services/db.json'
 
 const container = {
-
+  display: 'flex'
 }
 
 const styles = {
   card: {
-    cursor: 'pointer',
     minWidth: 275,
     textAlign: 'center'
   },
@@ -45,27 +44,27 @@ function SimpleCard(props) {
           game={game}
           className={classes.card}>
           <CardContent>
-            <Typography className={classes.title} color="textSecondary">
+            {/* <Typography className={classes.title} color="textSecondary">
               Games and Keys
-          </Typography>
+      </Typography> */}
             <img
               alt={game.name}
               src={game.backgroundImageURL}
               style={{ width: 400 }}
             />
-            <Typography variant="headline" component="h2">
+            <Typography variant="headline" component="h2" style={{ fontWeight: 700 }}>
               {game.name}
             </Typography>
             {/*<Typography className={classes.pos} color="textSecondary">
               {`Original Composer(s): ${game.info.originalComposer}`}
       </Typography> */}
-            <Typography component="p">
+            {/*<Typography component="p">
               Link to Sheet Music <br />
               Link to Midi File
-          </Typography>
+    </Typography> */}
           </CardContent>
           <CardActions>
-            <Button size="small">Downloads</Button>
+            <Button size='small' style={{ margin: '0 auto' }}>Downloads</Button>
           </CardActions>
         </Card>
       ))}
