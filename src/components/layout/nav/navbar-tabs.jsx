4cec609576f3ac5eb04fbdf818533d23b2d1ee2f
Joe Warren <joe.warren7@protonmail.com>
2019-05-11 10:48:52 -0500

    chore: replace react router with reach

--- src/components/layout/nav/navbar-tabs.jsx
+++ src/components/layout/nav/navbar-tabs.jsx
@@ -1,6 +1,6 @@
 // EXTERNAL DEPENDENCIES
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from '@reach/router';
 import PropTypes from 'prop-types';
 
 // MATERIAL-UI DEPENDENCIES
