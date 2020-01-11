ff219b3424fdd4a73489d9f16b17dfa47ea07408
Joe Warren <joe.warren7@protonmail.com>
2020-01-11 01:19:04 -0600

    fix: fix gap between navbar and side drawer on mobile horizontal (toggle fixed navbar based on route

--- src/components/docs/docs-side-drawer.jsx
+++ src/components/docs/docs-side-drawer.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+import { Introduction, QuickStart, Synthesia, Downloading } from './docs-config';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		display: 'flex',
+		display: 'flex'
 	},
 	drawer: {
 		[theme.breakpoints.up('sm')]: {
@@ -40,7 +38,7 @@ const useStyles = makeStyles(theme => ({
 	toolbar: theme.mixins.toolbar,
 	drawerPaper: {
 		width: drawerWidth,
-		zIndex: '-1'
+		marginTop: 64
 	},
 	content: {
 		flexGrow: 1,
@@ -52,29 +50,22 @@ function DocsSideDrawer(props) {
 	const { container } = props;
 	const classes = useStyles();
 	const theme = useTheme();
-	const [mobileOpen, setMobileOpen] = React.useState(false);
+	const [mobileOpen, setMobileOpen] = useState(false);
+	const [currentDoc, setCurrentDoc] = useState('Introduction');
 
 	const handleDrawerToggle = () => {
 		setMobileOpen(!mobileOpen);
 	};
 
+	const handleClickDocTitle = doc => {
+		setCurrentDoc(doc);
+	};
+
 	const drawer = (
 		<div>
-			<div className={classes.toolbar} />
-			<Divider />
 			<List>
-				{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-					<ListItem button key={text}>
-						<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-						<ListItemText primary={text} />
-					</ListItem>
-				))}
-			</List>
-			<Divider />
-			<List>
-				{['All mail', 'Trash', 'Spam'].map((text, index) => (
-					<ListItem button key={text}>
-						<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+				{['Introduction', 'Quick Start', 'Downloading', 'Synthesia'].map(text => (
+					<ListItem button key={text} onClick={() => handleClickDocTitle(text)}>
 						<ListItemText primary={text} />
 					</ListItem>
 				))}
@@ -120,27 +111,13 @@ function DocsSideDrawer(props) {
 			<main className={classes.content}>
 				<div className={classes.toolbar} />
 				<Typography paragraph>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-				</Typography>
-				<Typography paragraph>
-					Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+					{/* cleanup in refactor */}
+					{currentDoc === 'Introduction' ? <Introduction />
+						: currentDoc === 'Quick Start' ? <QuickStart />
+							: currentDoc === 'Synthesia' ? <Synthesia />
+								: currentDoc === 'Downloading' ? <Downloading />
+									: null
+					}
 				</Typography>
 			</main>
 		</div>
@@ -149,9 +126,9 @@ function DocsSideDrawer(props) {
 
 DocsSideDrawer.propTypes = {
 	/**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
+	 * Injected by the documentation to work in an iframe.
+	 * You won't need it on your project.
+	 */
 	container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element),
 };
 
