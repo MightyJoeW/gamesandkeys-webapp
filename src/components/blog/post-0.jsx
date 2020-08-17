f77f488fd8554b86d39d4782cb6eb98c9120febe
Joe Warren <joe.warren7@protonmail.com>
2020-08-17 15:11:42 -0500

    fix: make font bigger on blog posts and decrease width

--- src/components/blog/post-0.jsx
+++ src/components/blog/post-0.jsx
@@ -4,12 +4,16 @@ import { makeStyles } from '@material-ui/core/styles';
 const Post0 = () => {
 	const useStyles = makeStyles(theme => ({
 		container: {
-			width: '50vw',
+			width: '40vw',
 			margin: '0 auto 45px auto',
 			padding: 15,
 			[theme.breakpoints.down('sm')]: {
 				width: '90vw'
 			},
+		},
+		text: {
+			lineHeight: 1.75,
+			fontSize: '1.2em'
 		},
 		link: {
 			color: '#b17acc',
@@ -20,17 +24,19 @@ const Post0 = () => {
 	return (
 		<div className={classes.container}>
 			<h1> New GameAndKeys YouTube Channel </h1>
-			<img src="static/blog-0-images/2020-01-12-gak-yt.png" alt="GamesAndKeys YouTube Channel" height="auto" width="100%" />
-			<p>I have created a new channel that is dedicated 100% to videogame piano tutorials <a className={classes.link} href="https://www.youtube.com/channel/UC4bMF2-gYaHNfVg0BIRlmtQ" target="_blank" rel="noopener noreferrer"><em>here</em></a>. This channel is dedicated solely to video game piano tutorials.</p>
+			<div className={classes.text}>
+				<img src="static/blog-0-images/2020-01-12-gak-yt.png" alt="GamesAndKeys YouTube Channel" height="auto" width="100%" />
+				<p>I have created a new channel that is dedicated 100% to videogame piano tutorials <a className={classes.link} href="https://www.youtube.com/channel/UC4bMF2-gYaHNfVg0BIRlmtQ" target="_blank" rel="noopener noreferrer"><em>here</em></a>. This channel is dedicated solely to video game piano tutorials.</p>
 
-			<p>Previously, I utilized my <a className={classes.link} href="https://www.youtube.com/mightyjoew" target="_blank" rel="noopener noreferrer"><em>mightyjoew</em></a> channel to upload the tutorials, but I also uploaded lots of other content including remixes, performances, songs, skits, podcasts, reviews, and tech talks.</p>
+				<p>Previously, I utilized my <a className={classes.link} href="https://www.youtube.com/mightyjoew" target="_blank" rel="noopener noreferrer"><em>mightyjoew</em></a> channel to upload the tutorials, but I also uploaded lots of other content including remixes, performances, songs, skits, podcasts, reviews, and tech talks.</p>
 
-			<p>I did find that fans of certain types of content enjoyed my other content, but creating a channel specifically for the video game piano tutorials will make it a faster, cleaner process for those only interested in that content.</p>
+				<p>I did find that fans of certain types of content enjoyed my other content, but creating a channel specifically for the video game piano tutorials will make it a faster, cleaner process for those only interested in that content.</p>
 
-			<p>As of this post, I have reuploaded everything existing tutorial to this channel. When I create new video game piano tutorials, it will be uploaded to the <a className={classes.link} href="https://www.youtube.com/channel/UC4bMF2-gYaHNfVg0BIRlmtQ" target="_blank" rel="noopener noreferrer"><em>GamesAnyKeys YouTube channel</em></a>.</p>
+				<p>As of this post, I have reuploaded everything existing tutorial to this channel. When I create new video game piano tutorials, it will be uploaded to the <a className={classes.link} href="https://www.youtube.com/channel/UC4bMF2-gYaHNfVg0BIRlmtQ" target="_blank" rel="noopener noreferrer"><em>GamesAnyKeys YouTube channel</em></a>.</p>
 
-			<p>I will check comments from GamesAndKeys and MightyJoeW, so I'll still see your feedback if you happen to comment on the old channel.</p>
-		</div >
+				<p>I will check comments from GamesAndKeys and MightyJoeW, so I'll still see your feedback if you happen to comment on the old channel.</p>
+			</div >
+		</div>
 	);
 };
 
--- src/components/blog/post-2.jsx
+++ src/components/blog/post-2.jsx
@@ -4,12 +4,16 @@ import { makeStyles } from '@material-ui/core/styles';
 const Post2 = () => {
   const useStyles = makeStyles(theme => ({
     container: {
-      width: '50vw',
+      width: '40vw',
       margin: '0 auto 45px auto',
       padding: 15,
       [theme.breakpoints.down('sm')]: {
         width: '90vw'
       }
+    },
+    text: {
+      lineHeight: 1.75,
+      fontSize: '1.2em'
     },
     link: {
       color: '#b17acc',
@@ -21,17 +25,19 @@ const Post2 = () => {
     <div className={classes.container}>
       <h1> Sheet Music Issue Fixed for Mobile Devices </h1>
 
-      <img src="static/blog-2-images/the-choice.png" alt="The Last of Us Main Theme, Piano Tutorial YouTube Thumbnail" height="auto" width="100%" />
+      <div className={classes.text}>
+        <img src="static/blog-2-images/the-choice.png" alt="The Last of Us Main Theme, Piano Tutorial YouTube Thumbnail" height="auto" width="100%" />
 
-      <p>The long-time issue of attempting to view and download sheet music files on mobile devices has finally been resolved. </p>
+        <p>The long-time issue of attempting to view and download sheet music files on mobile devices has finally been resolved. </p>
 
-      <p>The simple change from png files to pdf files stops the multi-page tutorials from automatically rotating.</p>
-
-
-      <p>With the fix, you can now scroll the pdf files as you would a normal file or webpage.</p>
+        <p>The simple change from png files to pdf files stops the multi-page tutorials from automatically rotating.</p>
 
 
-      <p>If you have any continued issues with viewing or downloading the sheet music files, contact me at themightyjoew@gmail.com or create an issue on <a className={classes.link} href="https://github.com/MightyJoeW/gamesandkeys-webapp/issues" target="_blank" rel="noopener noreferrer"><em>GitHub</em></a>.</p>
+        <p>With the fix, you can now scroll the pdf files as you would a normal file or webpage.</p>
+
+
+        <p>If you have any continued issues with viewing or downloading the sheet music files, contact me at themightyjoew@gmail.com or create an issue on <a className={classes.link} href="https://github.com/MightyJoeW/gamesandkeys-webapp/issues" target="_blank" rel="noopener noreferrer"><em>GitHub</em></a>.</p>
+      </div>
     </div>
   );
 };
