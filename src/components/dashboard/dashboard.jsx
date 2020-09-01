7c6062841b2dd56f9c5bce0b0375f403136b64c8
Joe Warren <joe.warren7@protonmail.com>
2020-09-01 00:58:03 -0500

    test: setup rtl and add dashboard assertions

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -25,12 +25,14 @@ const Dashboard = () => {
 	}));
 	const classes = useStyles();
 	useEffect(() => {
-		ReactGA.initialize(`UA-${trackingId}-01`, {
-			debug: false // set to true to log pageview to console
-			// I also have the chrome extension with localhost/* whitelisted
+		if (process.env.NODE_ENV === 'production') {
+			ReactGA.initialize(`UA-${trackingId}-01`, {
+				debug: false // set to true to log pageview to console
+				// I also have the chrome extension with localhost/* whitelisted
 
-		});
-		ReactGA.pageview(window.location.pathname + window.location.search);
+			});
+			ReactGA.pageview(window.location.pathname + window.location.search);
+		}
 	}, []);
 
 	const trackEvent = (title) => {
--- src/components/dashboard/dashboard.test.js
+++ src/components/dashboard/dashboard.test.js
@@ -1,5 +1,18 @@
-describe('Dashboard tests', () => {
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect'
+import Dashboard from './dashboard';
+
+describe('Dashboard', () => {
 	it('should render the Dashboard component', () => {
-		expect(true);
+		render(<Dashboard />)
+		// screen.debug();
+		expect(screen.getByText('Play your favorite video game songs on the piano')).toBeInTheDocument();
+		// expect(screen.getByText('GamesAndKeys is a free learning resource that teaches pianists how to play songs from video games and tv shows via ')).toBeInTheDocument();
+		expect(screen.getByRole('img')).toBeInTheDocument();
+		expect(screen.getByRole('link')).toBeInTheDocument();
+		expect(screen.getByRole('separator')).toBeInTheDocument();
+		expect(screen.getByAltText('Synthesia Piano Tutorial')).toBeInTheDocument();
 	});
 });
